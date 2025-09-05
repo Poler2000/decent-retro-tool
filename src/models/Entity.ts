e752905b354ef9889fd2d2120f3b53999809ba36
Pawel Polerowicz <p.polerowicz@gmail.com>
2025-09-05 22:46:03 +0200

    add missing files

---
+++ src/models/Entity.ts
@@ -0,0 +1,4 @@
+export default interface Entity {
+  readonly id: number
+  readonly name: string,
+}
---
+++ src/retroClient.ts
@@ -0,0 +1,4 @@
+import type RetroModel from "./models/RetroModel";
+
+export const getRetros = async (teamId: number): Promise<RetroModel[]> => {
+}
